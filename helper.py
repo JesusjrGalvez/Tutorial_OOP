0e727f22778f672e4c3932bc8f4d840d703e3c96
Jesus Galvez <jssglvz@gmail.com>
2022-10-01 13:12:51 -0500

    Commit

--- helper.py
+++ helper.py
@@ -1,5 +1,4 @@
 #import pandas as pd
 
-print("hello world")
 #a = pd.Series([1, 2, 3])
 #print(a)
--- main.py
+++ main.py
@@ -4,6 +4,21 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import csv
 import helper
-import polygons
+import hr
+#import polygons
+#import polygons_final
+
+
+
+salary_employee = hr.SalaryEmployee(1, 'John Smith', 1500)
+sr = hr.SalaryEmployee(4, "Patty", 2000)
+hourly_employee = hr.HourlyEmployee(2, 'Jane Doe', 40, 15)
+commission_employee = hr.CommissionEmployee(3, 'Kevin Bacon', 1000, 250)
+payroll_system = hr.PayrollSystem()
+payroll_system.calculate_payroll([
+    salary_employee,
+    hourly_employee,
+    commission_employee
+])
 
 
